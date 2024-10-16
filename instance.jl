837d4692884268ada08619f14b40db45da79a92a
Gabriel Alves <gabrielcorreia@gmail.com>
2024-10-16 17:44:25 -0300

    Now you can open an instance locally, you can create a random instance file, or just create a random instance that won't be saved to a file.

--- instance.jl
+++ instance.jl
@@ -9,6 +9,45 @@ struct distance_matrix
     n::Int64
 end
 
+function create_instance(name)
+    
+    n1 = 5
+    rng = Random.MersenneTwister(1)
+    X1 = 5 * rand(rng, n1)
+    Y1 = 5 * rand(rng, n1)
+    d1 = [sqrt((X1[i] - X1[j])^2 + (Y1[i] - Y1[j])^2) for i in 1:n1, j in 1:n1]
+
+    open(name, "w") do io
+        println(io, n1)
+        for i in X1
+            println(io, i)
+        end
+        for j in Y1
+            println(io, j)
+        end
+
+        for i in 1:n1
+            for j in 1:n1
+                println(io, d1[i, j])
+            end
+        end
+    end
+
+end
+
+function load_instance(name)
+    open(name, "r") do file 
+        n1 = parse(Int, readline(file))
+
+        X1 = [parse(Float64, readline(file)) for i in 1:n1]
+        Y1 = [parse(Float64, readline(file)) for i in 1:n1]
+
+        d1 = [parse(Float64, readline(file)) for i in 1:(n1 * n1)]
+        d1 = reshape(d1, n1, n1)
+
+        return distance_matrix(X1, Y1, d1, n1)
+    end
+end
 function generate_distance()
     
     n1 = 5
--- run.jl
+++ run.jl
@@ -1,8 +1,34 @@
 include("model.jl")
 include("instance.jl")
 
-insta = Tsp_model.generate_distance()
-tour_edges, cost = build_tsp_model(insta)
+println("What you want to do (1 - Open archive) (2 - Make a random) (3 - Create a random archive) ?: ")     # The third is just for practice
+auxiliar = parse(Int, readline())
+
+if auxiliar == 1
+    println("Put the name of the archive(put '.txt'): ")
+    name = readline()
+
+    insta = Tsp_model.load_instance(name)
+    tour_edges, cost = build_tsp_model(insta)
+
+elseif auxiliar == 2
+
+    insta = Tsp_model.generate_distance()
+    tour_edges, cost = build_tsp_model(insta)
+
+elseif auxiliar == 3
+
+    println("Put the name of the archive(put '.txt'): ")
+    name = readline()
+
+    Tsp_model.create_instance(name)
+    insta = Tsp_model.load_instance(name)
+    tour_edges, cost = build_tsp_model(insta)
+
+else
+    println("Please enter a valid option")
+end
+
 
 open("tsp_solution.txt", "w") do io
     write(io, "Edges traveled:\n")
